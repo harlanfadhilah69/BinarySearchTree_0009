9407096254c3d1b5a754fc99b0e17a4660bc774a
harlanfadhilah69 <harlancoegg@gmail.com>
2024-06-13 19:52:14 +0700

    void preorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -86,6 +86,21 @@ public:
 		}
 	}
 
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empety" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
 	
 
 
